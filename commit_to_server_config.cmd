779c0df133f2ca72c8a48e1095cea979fd6c6ac6
Félix DENIS <felix.denis@outlook.com>
2025-09-17 23:15:37 +0200

    configure deployment

--- commit_to_server_config.cmd
+++ commit_to_server_config.cmd
@@ -2,7 +2,7 @@ cd C:\Users\felix\Documents\Projet\odoo\odoo19
 
 robocopy . ..\..\server_config\images\odoo\19\data /mir /XD ".git" /NP /MT:16
 
-cd ..\server_config\images\odoo\19\data
+cd ..\..\server_config\images\odoo\19\data
 git add .\*
 git commit -m "update project odoo odoo-19" -- .\
 git push
